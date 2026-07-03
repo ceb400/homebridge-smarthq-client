cfd6be89492662d0df7dc26b589634da707a774b
ceb400 <chuck.e.baumann@gmail.com>
2026-07-03 04:48:08 -0700

    Delete dist directory cleanup

--- dist/airConditionerServices.js
+++
@@ -1,5 +0,0 @@
-import { AirConditioner } from './airConditioner/airConditioner.js';
-export function setupAirConditionerServices(accessory, deviceServices, deviceId) {
-    new AirConditioner(this, accessory, deviceServices, deviceId);
-}
-//# sourceMappingURL=airConditionerServices.js.map
--- dist/dishwasherServices.d.ts
+++
@@ -1,4 +0,0 @@
-import { PlatformAccessory } from 'homebridge';
-import { SmartHqPlatform } from './platform.js';
-import { DeviceService } from 'ge-smarthq';
-export declare function setupDishwasherServices(this: SmartHqPlatform, accessory: PlatformAccessory, deviceServices: DeviceService[], deviceId: string, groupAccessory?: PlatformAccessory[]): void;
