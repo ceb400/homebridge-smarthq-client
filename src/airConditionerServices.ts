432ac4cac928e1b3ca7bfbbd030a24706899ac8e
actuallyevan <56329378+actuallyevan@users.noreply.github.com>
2026-07-12 20:40:04 -0700

    first working implementation

--- src/airConditionerServices.ts
+++ src/airConditionerServices.ts
@@ -3,6 +3,6 @@ import { PlatformAccessory }  from 'homebridge';
 import { SmartHqPlatform }    from './platform.js';
 import { DeviceService }      from 'ge-smarthq-api';
 
-export function setupAirConditionerServices(this: SmartHqPlatform, accessory: PlatformAccessory, deviceServices: DeviceService[], deviceId: string) {
-  new AirConditioner(this, accessory, deviceServices, deviceId);
+export function setupAirConditionerServices(this: SmartHqPlatform, accessory: PlatformAccessory, deviceServices: DeviceService[], deviceId: string, groupAccessory?: PlatformAccessory[]) {
+  new AirConditioner(this, accessory, deviceServices, deviceId, groupAccessory ?? []);
 }
